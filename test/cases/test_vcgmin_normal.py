0357dbfae53faa69d6271a943c5ca156512f1bfe
went_code <menfeifei@huawei.com>
2026-06-05 21:16:50 +0800

    [UT] add mskpp ut
    Co-authored-by: went_code<menfeifei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !54 merge master into master
    
    [UT] add mskpp ut
    
    Created-by: went_code
    Commit-by: went_code
    Merged-by: ascend-robot
    Description: ### 1. 修改描述
    - **修改原因：**  完善mskpp建模指令的覆盖UT
    - **修改方案：**  形成覆盖vector指令的UT
    - **修改内容：**  补充vabs\vaddrelu\vaddreluconv
    - [ ] **涉及代码双合**(贴上另一个PR链接)：不涉及
    ----
    
    ### 2. 功能验证
    - [ ] **功能自验截图**（请确保不体现个人信息）  
    ![image.png](https://raw.gitcode.com/user-images/assets/8858747/67298f5e-f2d5-4597-9bb6-98a0b5fc4aa5/image.png 'image.png')
    - [ ] **冒烟是否通过**  不涉及
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**  不涉及
    - [ ] **是否具备UT测试用例看护** 不涉及
    - [ ] **是否需要在sig会议中进行代码检视** 不涉及
    - **检视committer人员名单与检视时间：** 不涉及
    ----
    ### 4. 资料修改自检
    - **资料修改：**    不涉及
    ----
    
    See merge request: Ascend/mskpp!54

---
+++ test/cases/test_vcgmin_normal.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# -------------------------------------------------------------------------
+# This file is part of the MindStudio project.
+# Copyright (c) 2026 Huawei Technologies Co.,Ltd.
+#
+# MindStudio is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+#          http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# -------------------------------------------------------------------------
+import os
+import unittest
+from mskpp import vcgmin, Tensor, Chip
+from mskpp._C import task_schedule  # pylint: disable=all
+from test.utils.test_base import TestBase
+
+FP_16_TYPE = "FP16"
+FORMAT_TYPE = "ND"
+GM_MEM = "GM"
+UB_MEM = "UB"
+
+
+def my_vcgmin(gm_x, reduce_num):
+    ub_x = Tensor(UB_MEM)
+    ub_y = Tensor(UB_MEM)
+    ub_x.load(gm_x)
+    out = vcgmin(ub_x, ub_y, reduce_num)()
+    z = out[0]
+    return z
+
+
+class TestUtilsMethods(TestBase):
+    TRACE_FILE = 'trace.json'
+
+    def clean(self):
+        task_schedule.Schedule().clean()
+        work_dir = os.getcwd()
+        self.batch_delete_folders(work_dir, 'MSKPP_*')
+
+    def test_vcgmin_fp16_expect_trace_successful_create(self):
+        with Chip("Ascend910B1") as chip:
+            chip.enable_trace()
+            in_x_f16 = Tensor(GM_MEM, FP_16_TYPE, [4, 2048], format=FORMAT_TYPE)
+            in_y_f16 = Tensor(GM_MEM, FP_16_TYPE, [4, 2048], format=FORMAT_TYPE)
+            out_y1 = my_vcgmin(in_x_f16, 64)
+            in_y_f16.load(out_y1)
+            trace_file = os.path.join(chip.output_dir, self.TRACE_FILE)
+        self.assertTrue(os.path.exists(trace_file))
+        self.clean()
+
+
+if __name__ == '__main__':
+    unittest.main()
